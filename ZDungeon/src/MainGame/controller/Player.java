7050fe11c084b82ddbd00809ce2fe6465b89cec6
Mike G <38267949+Eyeraa@users.noreply.github.com>
2020-09-19 19:56:47 -0500

    Adding functionality to player

--- ZDungeon/src/MainGame/controller/Player.java
+++ ZDungeon/src/MainGame/controller/Player.java
@@ -1,15 +1,43 @@
 package MainGame.controller;
 
+import MainGame.ui.Consoleui;
+import MainGame.Game;
 public class Player {
+	private Game currentGame;
 	private String name;
 	private int level;
-	private int health;
+	private int health = 100;
 	private Weapon weapon;
 	
-	public Player() {
-		System.out.println();
+	public Player(Consoleui input, Game game) {
+		this.currentGame = game;
+		Consoleui ui = input;
+		//player initialization run from console!
+		this.name = ui.read();
+		
 		
 	}
 	
+	public int Attack(Weapon weapon, Monster monster)
+	{
+		//attack monster
+		//subtract life from monster
+		return 0;
+	}
 	
-}
+	public void Heal()
+	{
+		//rest text
+		health += 10;
+	}
+	
+	public void Death()
+	{
+		//show score after death! / other stats
+		currentGame.gameOver(); //game loop ended
+	}
+	
+	
+	
+	
+	
