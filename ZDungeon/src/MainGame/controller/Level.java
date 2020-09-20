96cea55430bb2a63535addfae7b6213ad8ab48e6
Mike G <38267949+Eyeraa@users.noreply.github.com>
2020-09-19 19:56:09 -0500

    Added functionality to level class
    giving Level class ability to be accessed by other classes
    summon monster and grandLoot still TODO

--- ZDungeon/src/MainGame/controller/Level.java
+++ ZDungeon/src/MainGame/controller/Level.java
@@ -1,5 +1,53 @@
 package MainGame.controller;
 
 public class Level {
-
+/** object of level class is to keep track of remaining loot, and remaining monsters
+ * as well as adjust monster difficulty, as well is item rarity TODO
+ */
+	String levelName = "PlaceHolder";
+	
+	
+	double lootValue;
+	double  MonsterDifficulty;
+	
+	String[] monsterNameList = 		{"bob","mike","arnold","raymond","jake","anthony"};
+	String[] monsterAttributeList = {"slime","snake","bull","tiger","demon","dragon"};
+	
+	String[] weapongNameList = 		{"Mace","knife","halberd","sickle","WarHammer","Cutlass"};
+	String[] weaponAttributeList =  {"wooden","stone","copper","steel","gold","platinum"};
+	
+	int remainingLoot;
+	int remainingMonsters;
+	public Level()
+	{
+		lootValue = 0;
+		MonsterDifficulty = 1;
+		
+		remainingLoot = 5;
+		remainingMonsters = 8;
+	}
+	
+	public void MonsterDeath()
+	{
+		remainingMonsters--;
+		MonsterDifficulty++;
+	}
+	
+	public void LootRetreived()
+	{
+		remainingLoot--;
+		lootValue++;
+	}
+	
+	public void SummonMonster()
+	{
+		
+	}
+	
+	public void GrantLoot()
+	{
+		
+	}
+	
+	
 }
